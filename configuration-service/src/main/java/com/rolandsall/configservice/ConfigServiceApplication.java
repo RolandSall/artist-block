185e1212aa8958cf726a725db5c65cc3798da8ad
Roland Salloum <roland.salloum00@outlook.com>
2022-03-22 19:09:32 +0200

    feat: config  to discovery connection

--- configuration-service/src/main/java/com/rolandsall/configservice/ConfigServiceApplication.java
+++ configuration-service/src/main/java/com/rolandsall/configservice/ConfigServiceApplication.java
@@ -2,11 +2,13 @@ package com.rolandsall.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
