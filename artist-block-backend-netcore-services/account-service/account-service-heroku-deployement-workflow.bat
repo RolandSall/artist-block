6f4227da0d44a6fb3af6be8868b807aae7e136e8
Roland Salloum <roland.salloum00@outlook.com>
2022-03-24 11:17:18 +0200

    fix: remove extra docker push argument

--- artist-block-backend-netcore-services/account-service/account-service-heroku-deployement-workflow.bat
+++ artist-block-backend-netcore-services/account-service/account-service-heroku-deployement-workflow.bat
@@ -2,4 +2,4 @@
 
 docker build -f staging.Dockerfile -t rolandsall24/artist-block-account-service:1.0.5 .
 
-docker push rolandsall24/artist-block-account-service:1.0.5 .
+docker push rolandsall24/artist-block-account-service:1.0.5
