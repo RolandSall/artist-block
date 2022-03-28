3819b6d76cd61e9868a73801100eb9cf4148f329
robz <robenkaraa29@gmail.com>
2022-03-28 23:33:39 +0300

    feat: try pipeline with broken test

---
+++ artist-block-backend-netcore-services/account-service-tests/Controller/Auth0TestController/Auth0TestControllerTests.cs
@@ -0,0 +1,27 @@
+using System;
+using account_service.Controllers.Auth0TestController;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+public class Auth0TestControllerTests
+{
+    
+    [Fact]
+    public void Public_Void_ReturnOkStatus()
+    {
+        // Arrange
+        var controller = new Auth0TestController();
+        
+        // Act
+        var returnedValue = controller.Public();
+        
+        // Assert
+        returnedValue.Should().BeOfType<OkObjectResult>();
+
+        var returned = (returnedValue as OkObjectResult).Value;
+
+        returned.Should()
+            .BeEquivalentTo("/ from a public endpoint! You don't need to be authenticated to see this!!!");
+    }
+}
--- artist-block-backend-netcore-services/account-service/Program.cs
+++ artist-block-backend-netcore-services/account-service/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Steeltoe.Discovery.Client;
 using Steeltoe.Extensions.Configuration.ConfigServer;
 using Steeltoe.Management.Endpoint;
