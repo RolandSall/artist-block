f10448ee29e72f41e17a4c49ec2f88c3be5bb9c7
Roland Salloum <roland.salloum00@outlook.com>
2022-03-30 17:49:19 +0300

    feat: added unit test for Painter registration endpoint

--- artist-block-backend-netcore-services/account-service-tests/Controller/UserRegistrationController/UserRegistrationControllerTests.cs
+++ artist-block-backend-netcore-services/account-service-tests/Controller/UserRegistrationController/UserRegistrationControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 using account_service.Controllers.Auth0TestController;
 using account_service.Profile.ClientProfile;
@@ -11,8 +12,10 @@ using Xunit;
 using account_service.Controllers.UserRegistrationController;
 using account_service.DTO.Registration;
 using account_service.Models;
+using account_service.Profile.PainterProfile;
 using account_service.Service.RegistrationService;
 using FluentAssertions;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
@@ -24,9 +27,13 @@ public class UserRegistrationControllerTests
     //private readonly Mock<IRegistrationRepo> _repositoryStub = new();
     // create an automapper instance for unit tests
     private readonly IMapper _mapperClient = new MapperConfiguration(mc => mc.AddProfile(new ClientProfile())).CreateMapper();
+    private static readonly List<Profile> profiles = new List<Profile>() { new ClientProfile(), new PainterProfile() };
+    private readonly IMapper _mapperPainter = new MapperConfiguration(mc => mc.AddProfiles(profiles)).CreateMapper();
+        
     private readonly Mock<IRegistrationService> _registrationServiceStub = new() ;
     private static readonly  DateTime _dateTime = DateTime.Now;
-    private readonly CreateClientDto _providedCreateDto = new CreateClientDto()
+    
+    private static readonly CreateClientDto _providedCreateClientDto = new CreateClientDto()
     {
         Email = "user@example.com",
         Nationality = "Nationality",
@@ -37,7 +44,15 @@ public class UserRegistrationControllerTests
         PhoneNumber = "1111",
     };
     
-    private readonly ReadClientDto _expectedReadDto = new ReadClientDto()
+    private readonly CreatePainterDto _providedCreatePainterDto = new CreatePainterDto()
+    {
+        CreateClientDto = _providedCreateClientDto,
+        Bio = "Very experienced at painting stuff",
+        Location = "Congo",
+        YearsOfExperience = "23+",
+    };
+    
+    private static readonly ReadClientDto _expectedClientReadDto = new ReadClientDto()
     {
         RegisteredUserId = Guid.NewGuid(),
         Email = "user@example.com",
@@ -49,15 +64,24 @@ public class UserRegistrationControllerTests
         PhoneNumber = "1111",
     };
 
-    //TODO: refactoring needed 
+    private readonly ReadPainterDto _expectedPainterReadDto = new ReadPainterDto()
+    {
+        PainterId = Guid.NewGuid(),
+        Bio = "Very experienced at painting stuff",
+        Location = "Congo",
+        YearsOfExperience = "23+",
+        readClientDto = _expectedClientReadDto,
+    };
+
+        //TODO: refactoring needed 
     [Fact]
     public void RegisterClient_NewCreateClientDto_ReturnsOkObject()
     {
         // Arrange
 
         // setup the stub
-        var returnedValueRegisteredUserStub = _mapperClient.Map<RegisteredUser>(_providedCreateDto);
-        returnedValueRegisteredUserStub.RegisteredUserId = _expectedReadDto.RegisteredUserId;
+        var returnedValueRegisteredUserStub = _mapperClient.Map<RegisteredUser>(_providedCreateClientDto);
+        returnedValueRegisteredUserStub.RegisteredUserId = _expectedClientReadDto.RegisteredUserId;
         
         _registrationServiceStub.Setup(
             service => service.RegisterClient( It.IsAny<RegisteredUser>() , It.IsAny<string>()))
@@ -80,13 +104,14 @@ public class UserRegistrationControllerTests
         };
         
         // Act
-        var returnedValue = controller.RegisterClient(_providedCreateDto);
+        var returnedValue = controller.RegisterClient(_providedCreateClientDto);
         var value = ( ReadClientDto ) (returnedValue as OkObjectResult).Value;
 
         // Assert
-        value.Should().BeEquivalentTo(_expectedReadDto , opt => opt.ComparingByMembers<ReadClientDto>());
+        value.Should().BeEquivalentTo(_expectedClientReadDto , opt => opt.ComparingByMembers<ReadClientDto>());
     }
 
+    //TODO: please refactor jesus
     [Fact]
     public void RegisterClient_AlreadyRegisteredClientDto_ThrowsUserAlreadyRegisteredException()
     {
@@ -114,9 +139,49 @@ public class UserRegistrationControllerTests
         };
         
         // Act
-        var returnedValue = controller.RegisterClient(_providedCreateDto);
+        var returnedValue = controller.RegisterClient(_providedCreateClientDto);
         
         // Assert
         returnedValue.Should().BeOfType<ConflictObjectResult>();
+    }
+
+    [Fact]
+    public void RegisterPainter_NewCreatePainterDto_ReturnsOkObject()
+    {
+        // Arrange
+        
+        // setup the stub
+        var returnedValueRegisteredPainterStub = _mapperPainter.Map<Painter>(_providedCreatePainterDto);
+        // setup ids
+        returnedValueRegisteredPainterStub.PainterId = _expectedPainterReadDto.PainterId;
+        returnedValueRegisteredPainterStub.RegisteredUser.RegisteredUserId = _expectedClientReadDto.RegisteredUserId;
+        
+        _registrationServiceStub.Setup(
+                service => service.RegisterPainter( It.IsAny<Painter>() , It.IsAny<string>()))
+            .Returns( returnedValueRegisteredPainterStub );
+        
+        // Create Mock for User 
+        var identity = new ClaimsIdentity();
+        identity.AddClaims(new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, "auth0|123123123123"),
+            new Claim(ClaimTypes.Name, "Roland Salloum"),
+        });
+        var user = new ClaimsPrincipal(identity);
+        
+        // Setup the controller with out Mocked User
+        var controllerContext = new ControllerContext { HttpContext = new DefaultHttpContext { User = user } };
+        var controller = new account_service.Controllers.UserRegistrationController.UserRegistrationController(_mapperPainter,_registrationServiceStub.Object)
+        {
+            ControllerContext = controllerContext,
+        };
+        
+        // Act
+
+        var returnedReadPainterDto = controller.RegisterPainter( _providedCreatePainterDto ); 
+        var value = ( ReadPainterDto ) (returnedReadPainterDto as OkObjectResult).Value;
+
+        // Assert
+        value.Should().BeEquivalentTo(_expectedPainterReadDto, opt => opt.ComparingByMembers<ReadPainterDto>());
     }
 }
