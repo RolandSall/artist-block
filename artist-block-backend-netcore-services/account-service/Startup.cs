6a31a464401c2f39bd5b52c9471901ee71d3bc31
Roland Salloum <roland.salloum00@outlook.com>
2022-04-02 09:17:43 +0300

    fix: missing injection for speciality repo and service

--- artist-block-backend-netcore-services/account-service/Startup.cs
+++ artist-block-backend-netcore-services/account-service/Startup.cs
@@ -1,8 +1,11 @@
 ﻿using System.Security.Claims;
 using System.Text.Json.Serialization;
+using account_service.Models;
 using account_service.Repository;
 using account_service.Repository.RegistrationRepo;
+using account_service.Repository.SpecialityRepo;
 using account_service.Service.RegistrationService;
+using account_service.Service.SpecialityService;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
@@ -32,6 +35,8 @@ namespace account_service{
                 x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
             services.AddScoped<IRegistrationService, RegistrationService>();
             services.AddScoped<IRegistrationRepo, RegistrationRepo>();
+            services.AddScoped<ISpecialityRepo, SpecialityRepo>();
+            services.AddScoped<ISpecialityService, SpecialityService>();
             
             services.AddCors(options => {
                 options.AddPolicy(name: _CORSPolicy,
