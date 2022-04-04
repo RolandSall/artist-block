69c6fb7a80e1a73a659e645d48a4fcb03880d420
Roland Salloum <roland.salloum00@outlook.com>
2022-04-04 18:23:00 +0300

    feat: check collection for bought client

---
+++ artist-block-backend-netcore-services/account-service/Service/CollectionService/ICollectionService.cs
@@ -0,0 +1,10 @@
+﻿using account_service.Models;
+using account_service.ValueObjects;
+
+namespace account_service.Service.CollectionService;
+
+public interface ICollectionService
+{
+    IEnumerable<Painting> GetCurrentLoggedInUserPaintingCollection(string auth0UserId);
+    IEnumerable<Painting> GetPaintingCollectionByUserId(Guid userId);
+}
