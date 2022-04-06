8592221a68d111b4882cbd934a6664584d5a25b2
Roland Salloum <roland.salloum00@outlook.com>
2022-04-06 12:01:10 +0300

    feat: migration files

---
+++ artist-block-backend-netcore-services/account-service/Migrations/20220405081003_gan-image-entity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace account_service.Migrations
+{
+    public partial class ganimageentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "gan_image",
+                columns: table => new
+                {
+                    PK_gan_image_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    gan_image_description = table.Column<string>(type: "text", nullable: false),
+                    FK_painting_registered_user_id = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_gan_image", x => x.PK_gan_image_id);
+                    table.ForeignKey(
+                        name: "FK_gan_image_client_FK_painting_registered_user_id",
+                        column: x => x.FK_painting_registered_user_id,
+                        principalTable: "client",
+                        principalColumn: "PK_registered_user_id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_gan_image_FK_painting_registered_user_id",
+                table: "gan_image",
+                column: "FK_painting_registered_user_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "gan_image");
+        }
+    }
+}
---
+++ artist-block-backend-netcore-services/account-service/Migrations/20220405082032_gan-image-fix-url-field.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace account_service.Migrations
+{
+    public partial class ganimagefixurlfield : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "gan_image_url",
+                table: "gan_image",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "gan_image_url",
+                table: "gan_image");
+        }
+    }
+}
