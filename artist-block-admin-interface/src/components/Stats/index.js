fb58e59bab3fb80c3b8005860c5ad767ead464fe
Roland Salloum <roland.salloum00@outlook.com>
2022-04-17 11:08:36 +0300

    feat: temp bar and pie chart

--- artist-block-admin-interface/src/components/Stats/index.js
+++ artist-block-admin-interface/src/components/Stats/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import {useNavigate} from "react-router-dom";
 import CustomMap from "../Charts/map";
-import {Typography} from "@mui/material";
+import {Grid, Typography} from "@mui/material";
+import CustomPie from "../Charts/pie";
+import CustomRadar from "../Charts/radar";
+import CustomBar from "../Charts/radar";
 
 
 
@@ -17,6 +20,32 @@ const View1 = () => {
             "value": 1
         },
     ]
+
+const   piData =     [
+        {
+            "id": "Painter",
+            "label": "Painter",
+            "value": 346,
+            "color": "hsl(13, 70%, 50%)"
+        },
+            {
+                "id": "Client",
+                "label": "Client",
+                "value": 421,
+                "color": "hsl(229, 70%, 50%)"
+            },
+    ]
+
+const barData =    [
+    {
+        "Painting Type": "Painting",
+        "GAN": 42,
+        "hot dogColor": "hsl(314, 70%, 50%)",
+        "Real Painting": 108,
+        "burgerColor": "hsl(122, 70%, 50%)",
+    },
+
+        ]
     let navigate = useNavigate();
     return (
      <div   style={{height: 500}}>
@@ -25,6 +54,18 @@ const View1 = () => {
          </Typography>
          <br/>
          <CustomMap data={data}/>
+         <Grid container spacing={2}>
+             <Grid item xs={6}>
+                 <div   style={{height: 500}}>
+                 <CustomPie data={piData}/>
+                 </div>
+             </Grid>
+             <Grid item xs={6}>
+                 <div   style={{height: 500}}>
+                 <CustomBar data={barData}/>
+                 </div>
+             </Grid>
+         </Grid>
 
           </div>
 
