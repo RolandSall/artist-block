3bece6f0062f2682ae599bd1feb74d54011c998b
Roland Salloum <roland.salloum00@outlook.com>
2022-04-23 03:00:26 +0300

    fix: missing migration files

---
+++ artist-block-backend-netcore-services/account-service/Migrations/20220422235536_bought-timestamp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace account_service.Migrations
+{
+    public partial class boughttimestamp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "bought_timestamp",
+                table: "painting",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "bought_timestamp",
+                table: "painting");
+        }
+    }
+}
