cd7a04e75b357ad3724038d77bfac438a0612e91
Roland Salloum <roland.salloum00@outlook.com>
2022-05-23 22:01:40 +0300

    fix: bat file versions

--- artist-block-backend-netcore-services/account-service/account-service-heroku-deployement-workflow.bat
+++ artist-block-backend-netcore-services/account-service/account-service-heroku-deployement-workflow.bat
@@ -1,5 +1,5 @@
 ﻿echo "Starting"
 
-docker build -f staging.Dockerfile -t rolandsall24/artist-block-account-service:2.0.0 .
+docker build -f staging.Dockerfile -t rolandsall24/artist-block-account-service:4.0.0 .
 
-docker push rolandsall24/artist-block-account-service:2.0.0
+docker push rolandsall24/artist-block-account-service:4.0.0
